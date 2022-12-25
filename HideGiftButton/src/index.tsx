9fc9dd06a747d5ab0506deb292d91597cdb23294
ssense1337 <28709368+ssense1337@users.noreply.github.com>
2022-12-25 23:50:23 +0700

    HideGiftButton: fix for Discord v158.0
    Removed:
    - Settings

--- HideGiftButton/src/index.tsx
+++ HideGiftButton/src/index.tsx
@@ -2,66 +2,27 @@ import { Plugin, registerPlugin } from 'enmity/managers/plugins';
 import { create } from 'enmity/patcher';
 import manifest from '../manifest.json';
 
-import { React } from 'enmity/metro/common';
-import Settings from './components/Settings';
-
-import { View } from 'enmity/components';
+import { getByProps } from 'enmity/metro';
 import { findInReactTree } from 'enmity/utilities';
-import { getIDByName } from 'enmity/api/assets';
 
 const Patcher = create('hide-gift-button');
 
-// /assets/modules/chat_input/native/images/ic_gift.png
-const ic_gift = getIDByName('ic_add_24px') + 2; // 662
-const animVal = { value: 1 };
-
-import { getBoolean as _getBoolean } from 'enmity/api/settings';
-const getBoolean = (setting: string, defaults?: boolean) => _getBoolean(manifest.name, setting, defaults);
+const ChatInputWrapper = getByProps('ChatInput');
 
 const HideGiftButton: Plugin = {
    ...manifest,
 
    onStart() {
-      const unpatchView = Patcher.after(View, 'render', (ctx, [props], res) => {
-         const chatInput: any = findInReactTree(res, r => Array.isArray(r?.props?.actions));
+      Patcher.after(ChatInputWrapper.ChatInput.prototype, 'render', (ctx, [props], res) => {
+         const chatInput: any = findInReactTree(res, r => typeof r.props?.hideGiftButton === 'boolean');
          if (!chatInput) return;
 
-         Patcher.after(chatInput.type, 'render', (ctx, [props], res) => {
-            if (props.isChatInputPolishEnabled) return;
-            const [leftBtnsContainer, textInput, sendBtn] = res.props.children;
-
-            if (getBoolean('hide-gift', true)) {
-               const leftBtns = leftBtnsContainer?.props?.children;
-               if (!Array.isArray(leftBtns)) return;
-
-               const [actionsBtns, moreBtn] = leftBtns;
-
-               // Hide More Button
-               actionsBtns.props.animation = animVal;
-               actionsBtns.props.animateShowingActionButtons = true;
-               moreBtn.props.animation = animVal;
-
-               const actions = actionsBtns.props.actions;
-               const index = actions.findIndex(x => x.source === ic_gift);
-               if (index === -1) return;
-
-               actions.splice(index, 1);
-            }
-
-            if (getBoolean('always-show-send', true)) {
-               sendBtn.props.animation = animVal;
-            }
-         });
-         unpatchView();
+         chatInput.props.hideGiftButton = true;
       });
    },
 
    onStop() {
       Patcher.unpatchAll();
-   },
-
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
    }
 };
 
